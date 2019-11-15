a348d4ef82a061012788f6e7d66234c731f6dc97
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-11-15 08:34:37 +0000

    Recieiving data added basic styles and 2 elements for each card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import CardList from './components/CharacterList';
+import  { Route} from 'react-router-dom';
 
 export default function App() {
   return (
     <main>
+      <Route exact path='/' component={CardList} />
       <Header />
     </main>
   );
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,18 @@
 import React from "react";
+import StyledCard from './StyledCard';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+
+const CharacterCard = (props)  => {
+
+
+  return (
+    
+    <StyledCard>
+    <h1>{props.people.name}</h1>
+  <h2>gender:{props.people.gender}</h2>
+ 
+      </StyledCard>
+  );
 }
+
+export default CharacterCard;
