ed59ad65ed31189d9ba3389e49b6ddda1f1c1c20
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-11-15 09:05:47 +0000

    Working on Search added route  for Search form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import Header from "./components/Header.js";
 import CardList from './components/CharacterList';
 import WelcomePage from './components/WelcomePage';
-import  { Route} from 'react-router-dom';
+import SearchForm from './components/SearchForm';
+
+import  { Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <main>
-      
+      <nav>
+        <ul>
+          <li>link</li>
+        </ul>
+      </nav>
       <Route exact path='/' component={WelcomePage} />
       <Route exact path='/Characters' component={CardList} />
+      <Route exact path='/Search' component={SearchForm} />
       <Route exact path='/Header' component={Header} />
       
     </main>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import CharacterCard from './CharacterCard';
+import axios from "axios";
 
-export default function SearchForm() {
- 
+
+const SearchForm = (props) => {
+  
+  const [form, changeForm] = useState('');
+  const [result, submitResult] = useEffect ([]);
+
+
+  useEffect (() => {
+    axios.get('https://rickandmortyapi.com/api/character/')
+    .then (responce => {
+      console.log(responce.data.results);
+    })
+  })
+
+
   return (
     <section className="search-form">
-     // Add a search form here
+     
     </section>
   );
 }
+
+export default SearchForm;
