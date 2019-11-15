712da28023bdddb95bba6694da206f0902d73653
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-11-15 08:49:44 +0000

    Added routes in AppJs also added more elements to card + img

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CardList from './components/CharacterList';
+import WelcomePage from './components/WelcomePage';
 import  { Route} from 'react-router-dom';
 
 export default function App() {
   return (
     <main>
-      <Route exact path='/' component={CardList} />
-      <Header />
+      
+      <Route exact path='/' component={WelcomePage} />
+      <Route exact path='/Characters' component={CardList} />
+      <Route exact path='/Header' component={Header} />
+      
     </main>
   );
 }
