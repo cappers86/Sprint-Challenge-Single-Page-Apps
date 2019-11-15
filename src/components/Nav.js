4921da065ffa2c41d2f6a68f6efbcd5385991d03
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-11-15 09:55:10 +0000

    Added working nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React from 'react';
+import StyledNav from './StyledNav';
 import  {  Link } from 'react-router-dom';
 
 export default function Nav() {
     return (
-        <nav>
         
+        
+        <StyledNav>
         <Link to="/">Home</Link>
         
         <Link to="/Characters">Card List</Link>
         
         <Link to="/Search">Search</Link>
-        
-      </nav>
+        </StyledNav>
+     
+      
     );
   }
   
--- src/components/StyledNav.js
+++ src/components/StyledNav.js
@@ -3,9 +3,28 @@ import styled from 'styled-components';
 
 
 const StyledNav = styled.nav`
-  display
+max-width: 120rem;
+display: flex;
+text-decoration: none;
+margin: auto;
+padding: 0 2rem;;
+justify-content: space-between;
+height: 5rem;
 
-  
+& a {
+    color: #dfe6e9;
+    text-transform: uppercase;
+    font-weight: 600;
+    border-bottom: 1px solid transparent;
+    margin: 0 1.5rem;
+    transition: all 300ms linear 0s;
+    text-decoration: none;
+    cursor: pointer;
+
+    &:hover {
+      color: #fdcb6e;
+     
+    }
    
 `;
 
